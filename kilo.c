599986f9504257735e0579d6bf07fc2d57815508
Tiger Abrodi <tigerabrodi@gmail.com>
2023-11-03 09:55:30 +0100

    ctrl key and q quit

--- kilo.c
+++ kilo.c
@@ -6,6 +6,9 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+#define CTRL_KEY(k) ((k)&0x1f)
+
 /*** data ***/
 struct termios orig_termios;
 
@@ -47,7 +50,6 @@ int main()
 		char c = '\0';
 		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
 			die("read");
-		read(STDIN_FILENO, &c, 1);
 		if (iscntrl(c))
 		{
 			printf("%d\r\n", c);
@@ -56,7 +58,7 @@ int main()
 		{
 			printf("%d ('%c')\r\n", c, c);
 		}
-		if (c == 'q')
+		if (c == CTRL_KEY('q'))
 			break;
 	}
 
